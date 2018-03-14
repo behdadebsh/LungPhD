46a5221983503f41af82991f61444b167c275e25
Behdad Shaarbaf Ebrahimi <behdad.ebrahimi@gmail.com>
2018-03-15 10:54:55 +1300

    Function for loading the DICOM images from Directory

--- src/load_scan.py
+++ src/load_scan.py
@@ -1,10 +1,30 @@
 import os
-
 import dicom
 import numpy as np
 
 
+def load_scan(path):
+    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+    slices.sort(key=lambda x: int(x.InstanceNumber))
+    try:
+        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
+    except:
+        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
+
+    for s in slices:
+        s.SliceThickness = slice_thickness
+
+    return slices
+
+
 def get_pixels_hu(scans):
+    """
+    method to convert to hounsfield units.
+
+
+    :param scans: slices from load_scan method.
+    :return: a numpy array with hounsfield values
+    """
     image = np.stack([s.pixel_array for s in scans])
     # Convert to int16 (from sometimes int16),
     # should be possible as values should always be low enough (<32k)
@@ -25,17 +45,3 @@ def get_pixels_hu(scans):
     image += np.int16(intercept)
 
     return np.array(image, dtype=np.int16)
-
-
-def load_scan(path):
-    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
-    slices.sort(key = lambda x: int(x.InstanceNumber))
-    try:
-        slice_thickness = np.abs(slices[0].ImagePositionPatient[2] - slices[1].ImagePositionPatient[2])
-    except:
-        slice_thickness = np.abs(slices[0].SliceLocation - slices[1].SliceLocation)
-
-    for s in slices:
-        s.SliceThickness = slice_thickness
-
-    return slices
