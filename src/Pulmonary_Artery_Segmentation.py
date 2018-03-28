65d251f1aa9f96fb5184422103b5481e1c19cd6c
Behdad Shaarbaf Ebrahimi <behdad.ebrahimi@gmail.com>
2018-03-29 10:34:40 +1300

    Pulmonary artery semi automatic segmentation.

--- src/Pulmonary_Artery_Segmentation.py
+++ src/Pulmonary_Artery_Segmentation.py
@@ -1,23 +1,48 @@
 import sys
+import matplotlib
+matplotlib.use('WX')
+from PIL import ImageTk, Image
+import Tkinter as tk
 import scipy.ndimage as ndimage
-import matplotlib.pyplot as plt
+import scipy.ndimage.filters as filters
+from mayavi import mlab
 import numpy as np
 from load_scan import load_scan, get_pixels_hu
 from Lvl_Set_Alg import lvlset
 
-# def vesselness(image):
-#     segmented, watershed, marker_int, marker_ext, marker_watershed, watershed_left, watershed_right = lung_segment(image)
-#
-#     vessels = segmented > 0
-#     vessel_mask = ndimage.binary_opening(vessels, iterations=1)
-#     vessels = image * vessel_mask
-#     print ("Vessel mask")
-#     plt.imshow(vessel_mask, cmap='gray')
-#     plt.show()
-#     print ("Vessel Segmentation")
-#     plt.imshow(vessels, cmap='gray')
-#     plt.show()
-#     return vessel_mask, vessels
+
+# def click_roi(event, x, y, flags, params):
+#     global roi_number, pts
+#     while roi_number <= 3:
+#         if event == cv2.EVENT_LBUTTONDOWN:
+#             pts.append((x, y))
+#             roi_number = roi_number + 1
+#         elif event == cv2.EVENT_LBUTTONDBLCLK:
+#             break
+#     print pts
+#     print pts[0, 0], pts[0, 1]
+#     return pts
+coords = []
+
+
+# def callback(event):
+#     print "clicked at", event.x, event.y
+
+
+def onclick(event):
+    # ix, iy = event.xdata, event.ydata
+    print 'x = %d, y = %d'%(
+        event.x, event.y)
+    global coords
+    coords.append((event.x, event.y))
+    return coords
+
+# def on_mouse(event, x, y, flags, params):
+#     if event == cv2.EVENT_LBUTTONDOWN:
+#         print 'Seed: ' + str(x) + ', ' + str(y)
+#         global ix, iy
+#         ix, iy = x, y
+
 
 def main():
     if len(sys.argv) > 1:
@@ -25,23 +50,50 @@ def main():
     # data_dir = '/hpc/bsha219/lung/Data/ST12/Raw/DICOMS'
     patient_scans = load_scan('/hpc/bsha219/lung/Data/ST12/Raw/DICOMS')
     patient_images = get_pixels_hu(patient_scans)
-    img = patient_images[150]
-    for i in range(512):
-        for j in range(512):
-            if img[i][j] > 0:
-                img[i][j] = img[i][j] * 20
-    blurred_f = ndimage.gaussian_filter(img, sigma=3)
-    filter_blurred_f = ndimage.gaussian_filter(blurred_f, sigma=1)
-    alpha = 70
-    sharpened = blurred_f + alpha * (blurred_f - filter_blurred_f)
+    global artery_mask
+    artery_mask = []
+    for k in range(147, 148):
+        global coords
+        coords = []
+        img = patient_images[k]
+        for i in range(img.shape[0]):
+            for j in range(img.shape[0]):
+                if img[i][j] > 0:
+                    img[i][j] *= 20
+        blurred_f = ndimage.gaussian_filter(img, sigma=3)
+        filter_blurred_f = ndimage.gaussian_filter(blurred_f, sigma=1)
+        filter_blurred_f2 = filters.median_filter(filter_blurred_f, 3)
+        alpha = 100
+        sharpened = blurred_f + alpha * (blurred_f - filter_blurred_f2)
 
-    mask = np.zeros(img.shape)
-    mask[300, 300] = 1
-    # plt.imshow(img, cmap='gray')
-    # plt.show()
-    lvlset(sharpened, mask, max_its=2000, display=True, alpha=0.2)
+        root = tk.Tk()
+        im = Image.fromarray(sharpened)
+        photo = ImageTk.PhotoImage(im)
+        canvas = tk.Canvas(root, width=512, height=512)
+        canvas.pack()
+        canvas.create_image(256, 256, image=photo)
+        canvas.bind("<Button-1>", onclick)
+        root.mainloop()
+
+        # fig = plt.figure()
+        # plt.gray()
+        # plt.imshow(img, cmap='gray')
+        # cid = fig.canvas.mpl_connect('button_press_event', onclick)
+        # plt.show()
+        # fig.canvas.mpl_disconnect(cid)
+        # time.sleep(10)
+        seed = np.zeros(img.shape)
+        for jj in range(len(coords)):
+            seed[int(coords[jj][1]), int(coords[jj][0])] = 1
+        seg, phi, its = lvlset(sharpened, seed, max_its=1000, display=True, alpha=0.2)
+        # fig.canvas.mpl_disconnect(cid)
+        # plt.close()
+        segment = seg * 1.0
+        artery_mask.append(segment)
+    artery_mask = np.asarray(artery_mask)
+    src = mlab.pipeline.scalar_field(artery_mask)
+    mlab.pipeline.volume(src, vmin=0, vmax=0.8)
 
 
 if __name__ == '__main__':
     main()
-
