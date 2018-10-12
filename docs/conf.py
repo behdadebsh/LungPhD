3b21d6c2f4a2eba7f0b3009d8314876f374dadbc
Behdad Shaarbaf Ebrahimi <behdad.ebrahimi@gmail.com>
2018-10-12 13:58:38 +1300

    Project logo added to _static and conf.py edited

--- docs/conf.py
+++ docs/conf.py
@@ -127,7 +127,7 @@ html_theme = 'alabaster'
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = 'Segmentation'
+html_short_title = 'Segmentation'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
