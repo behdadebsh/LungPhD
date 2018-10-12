44153360b952563edc1fac5eb74704f37e4cb7a9
Behdad Shaarbaf Ebrahimi <behdad.ebrahimi@gmail.com>
2018-10-12 13:57:01 +1300

    Project logo added to _static and conf.py edited

--- docs/conf.py
+++ docs/conf.py
@@ -127,11 +127,11 @@ html_theme = 'alabaster'
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
+#html_short_title = 'Segmentation'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = '_static/image.jpeg'
 
 # The name of an image file (relative to this directory) to use as a favicon of
 # the docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
