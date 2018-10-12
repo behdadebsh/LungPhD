8521f15b0a1d2dc7fae71261b6122027aa58149a
Behdad Shaarbaf Ebrahimi <behdad.ebrahimi@gmail.com>
2018-10-12 14:00:10 +1300

    Project logo added to _static and conf.py edited

--- docs/conf.py
+++ docs/conf.py
@@ -124,10 +124,10 @@ html_theme = 'alabaster'
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = 'Segmentation'
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = 'Segmentation'
+#html_short_title = None
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
