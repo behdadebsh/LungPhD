69d268c75a9c1f742e901064d05787127652546c
Behdad Shaarbaf Ebrahimi <behdad.ebrahimi@gmail.com>
2023-03-14 17:57:46 +1300

    Growing test from ages ago

--- src/Grow_into_Lungs.py
+++ src/Grow_into_Lungs.py
@@ -1,4 +1,3 @@
-
 from aether.diagnostics import set_diagnostics_on
 from aether.indices import define_problem_type
 from aether.geometry import define_node_geometry_2d, define_elem_geometry_2d, make_data_grid, evaluate_ordering, \
@@ -6,21 +5,24 @@ from aether.geometry import define_node_geometry_2d, define_elem_geometry_2d, ma
 from aether.exports import export_node_geometry_2d, export_elem_geometry_2d, export_data_geometry, export_node_geometry, \
     export_1d_elem_geometry
 from aether.growtree import grow_tree
-
+from time import sleep
 
 set_diagnostics_on(False)
 define_problem_type('grow_tree')
 
-define_node_geometry('/hpc/bsha219/Hari/Data/UCSDLungs/HNA_A03/Supine/Vessel/HNA_A03_UpperArtery.ipnode')
+define_node_geometry('/hpc/bsha219/lung/Data/CTEPH/CTEPH10/FTC/Vessel/CTEPH10_UpperArtery.ipnode')
 define_1d_elements('/hpc/bsha219/Hari/Data/UCSDLungs/HNA_A03/Supine/Vessel/HNA_A03_UpperArtery.ipelem')
 
 # Growing into RLL
-define_node_geometry_2d('/hpc/bsha219/Hari/Data/UCSDLungs/HNA_A03/Supine/Lung/SurfaceFEMesh/Right_fitted.ipnode')
-define_elem_geometry_2d('/hpc/bsha219/Hari/Data/UCSDLungs/HNA_A03/Supine/Vessel/RLL_surface.ipelem', 'unit')
+define_node_geometry_2d('/hpc/bsha219/lung/Data/CTEPH/CTEPH10/FRC/Lung/SurfaceFEMesh/Right_fitted.ipnode')
+define_elem_geometry_2d('/hpc/bsha219/lung/Data/UCSDLungs/HNA_A03/Supine/Vessel/RLL_surface.ipelem', 'unit')
 make_data_grid(0, 6.0, False, 'test', 'test')
+# export_data_geometry('wow','test',0)
+# sleep(5)
 evaluate_ordering()
 group_elem_parent_term(23)
 grow_tree(23, 1, 60.0, 20.0, 0.4, 1.5, 1.5, 180.0, False, 'test')
+# sleep(5)
 
 # Growing into RUL
 define_node_geometry_2d('/hpc/bsha219/Hari/Data/UCSDLungs/HNA_A03/Supine/Lung/SurfaceFEMesh/Right_fitted.ipnode')
