800ce342300b3eab9b6b5c166ad2f9c823d54b24
Behdad Shaarbaf Ebrahimi <behdad.ebrahimi@gmail.com>
2023-03-14 17:46:48 +1300

    extract surface data points from segmentation

--- src/surface_datacloud.py
+++ src/surface_datacloud.py
@@ -10,26 +10,19 @@ from PIL import Image
 import glob
 from utils import writeExDataFile
 
-# filelist_mpa = glob.glob('E:\lung\Data\Human_PE_Study_HRC\ST12\TLC\Vessel\MPA_Mask\*.png')
-filelist_mpa = glob.glob('E:\lung\Data\CTEPH\CTEPH6\FRC\Vessel\MPA2\*.jpg')
+filelist_mpa = glob.glob('/hpc/bsha219/lung/Data/Human_Lung_Atlas/P2BRP268-H12816/FRC/Vessel/*.jpg')
 filelist_mpa.sort()
 MPA_imgs = np.array([np.array(Image.open(fname)) for fname in filelist_mpa])
-# for slice in range(len(MPA_imgs)):
-#     mpimg.imsave(
-#         '/hpc/bsha219/lung/Data/Human_PE_Study_HRC/ST12/TLC/Vessel/whole_mask/FullMask%.4d.jpg' % slice,
-#         MPA_imgs[slice])
-# image = plt.imread(filelist_mpa[1])
-# print(image.shape[0])
 coords = []
 for slice_number in range(len(filelist_mpa)):
     image = plt.imread(filelist_mpa[slice_number])
     for y in range(image.shape[0]-1):
         FOUND = False
         for x in range(image.shape[1]-1):
-            if not FOUND and (int(image[x+1][y][0])) == 0 and (int(image[x][y][0])) == 1:
+            if not FOUND and (int(image[x+1][y])) == 1 and (int(image[x][y])) == 0:
                 FOUND = True
-                coords.append([x+1, y, slice_number])
-            elif FOUND and (int(image[x+1][y][0])) == 1 and (int(image[x][y][0])) == 0:
+                coords.append([(x+1)*0.65, y*0.65, slice_number*1.25])
+            elif FOUND and (int(image[x+1][y])) == 0 and (int(image[x][y])) == 1:
                 FOUND = False
-                coords.append([x, y, slice_number])
-writeExDataFile('E:\lung\Data\CTEPH\CTEPH6\FRC\Vessel\surface_MPAtrimmed.exdata', coords, mean_radius_field=None)
+                coords.append([x*0.65, y*0.65, slice_number*1.25])
+writeExDataFile('/hpc/bsha219/lung/Data/Human_Lung_Atlas/P2BRP268-H12816/Vessel/surface_MPAtrimmed.exdata', coords, mean_radius_field=None)
